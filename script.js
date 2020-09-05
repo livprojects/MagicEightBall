e91b19a2be96e9cf035ce71aac4694b49c100018
Liv <62610142+livprojects@users.noreply.github.com>
2020-09-05 21:03:46 +0200

    Added all files, still minor bugs to fix

---
+++ script.js
@@ -0,0 +1,65 @@
+const app = {
+
+    init: () => {
+        const question = document.getElementById('questionSubmitId');
+        console.log(question)
+        question.addEventListener('submit', app.magicWorks);
+
+    },
+
+    delayMagic: () => {
+        console.log('delayMagic is entered')
+    },
+
+    magicWorks: () => {
+
+
+        let randomNumber = Math.floor(Math.random() * 8);
+        let eightBall = '';
+
+        switch (randomNumber) {
+            case 0:
+                eightBall = 'It is certain';
+                break;
+            case 1:
+                eightBall = 'It is decidedly so';
+                break;
+            case 2:
+                eightBall = 'Reply hazy try again';
+                break;
+            case 3:
+                eightBall = 'Reply hazy try again';
+                break;
+            case 4:
+                eightBall = 'Cannot predict now';
+                break;
+            case 5:
+                eightBall = 'Do not count on it';
+                break;
+            case 6:
+                eightBall = 'My sources say no';
+                break;
+            case 7:
+                eightBall = 'Outlook not so good';
+                break;
+            case 8:
+                eightBall = 'Signs point to yes';
+                break;
+            default:
+                eightBall = `I'm not feeling it today`;
+                break;
+        }
+
+        const text = document.createElement("p"); 
+        console.log(eightBall);
+        const node = document.createTextNode(eightBall);
+        text.appendChild(node);
+
+        const container = document.getElementById('hereGoesTheAnswer');
+        container.appendChild(text);
+
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
